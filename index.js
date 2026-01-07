20c078d659cd77c63c50f12eb128c3590d0ba678
rochamariza <marizanfmrocha04@gmail.com>
2026-01-07 11:43:27 -0300

    Update 2

--- index.js
+++ index.js
@@ -5,35 +5,54 @@ function getComputerChoice(){
     const randomNumber = Math.floor(Math.random() * 2)
     if(randomNumber === 0){
         console.log("The computer chooses Rock")
-        return 0
+        return "rock"
     }else if(randomNumber === 1){
         console.log("The computer chooses Paper")
-        return 1
+        return "paper"
     }else{
         console.log("The computer chooses Scissor")
-        return 2
+        return "scissor"
     }
 }
 function getHumanChoice(){
-    const choice = prompt("Choose a number: 0 = Rock, 1 = Paper, 2 = Scissor")
-    const computerChoice = getComputerChoice()
-    if(choice == computerChoice){
-        return "Draw"
-    }else if(choice == 0 && computerChoice == 1){
-        computerChoice = computerChoice + 1
-        return "You lost!"
-    }else if(choice == 1 && computerChoice == 2){
-        computerChoice = computerChoice + 1
-        return "You lost!"
-    }else if(choice == 2 && computerChoice == 0){
-        computerChoice = computerChoice + 1
-        return "You lost!"
-    }else{
-        humanScore = humanScore + 1
-        return "You win!"
-    }
+    const choice = prompt("Write Rock, Paper or Scissor")
+    console.log(`You choose ${choice.slice(0, 1).toUpperCase() + choice.slice(1, choice.length)}`)
+    return choice.toLowerCase()
 }
 
 function playRound(humanChoice, computerChoice){
+    if(humanChoice == computerChoice){
+        console.log("Draw")
+        console.log(" ") 
+    }else if(humanChoice == "rock" && computerChoice == "paper"){
+        computerScore = computerScore + 1
+        console.log("You lost this round!")
+        console.log(" ") 
+    }else if(humanChoice == "paper" && computerChoice == "scissor"){
+        computerScore = computerScore + 1
+        console.log("You lost this round!")
+        console.log(" ") 
+    }else if(humanChoice == "scissor" && computerChoice == "rock"){
+        computerScore = computerScore + 1
+        console.log("You lost this round!")
+        console.log(" ") 
+    }else{
+        humanScore = humanScore + 1
+        console.log("You won this round!")
+        console.log(" ") 
+    }
+}
+function playGame(){
     
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice()
+        const computerSelection = getComputerChoice()
+        playRound(humanSelection, computerSelection)
+    }
+    if(humanScore > computerScore){
+        console.log("You won the Game!")
+    }else{
+        console.log("You lost the Game!")
+    }
+    return `Humans: ${humanScore} // Computer: ${computerScore}`
 }
